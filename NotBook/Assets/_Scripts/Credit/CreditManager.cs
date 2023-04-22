c08e96aeddf54b8bd1654f71800881aee8944ad4
Morgan Gasser <morgan.gasser@he-arc.ch>
2023-04-22 10:57:06 +0200

    creditUpdate

--- NotBook/Assets/_Scripts/Credit/CreditManager.cs
+++ NotBook/Assets/_Scripts/Credit/CreditManager.cs
@@ -1,71 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class CreditManager : MonoBehaviour
 {
     public float scrollSpeed = 50f;
-    public KeyCode resetKey = KeyCode.Space;
-    public float resetHoldTime = 2f;
-    public Text resetText;
     public float timerDuration = 10f;
 
     private Vector3 initialPosition;
-    private float keyHoldTimer = 0f;
-    private int dotCount = 0;
     private Coroutine timerCoroutine;
 
     void Start()
     {
         initialPosition = transform.position;
-        resetText.text = "Espace (Continuer)";
         timerCoroutine = StartCoroutine(TimerRoutine());
     }
 
     void Update()
     {
         transform.Translate(Vector3.up * scrollSpeed * Time.deltaTime);
-
-        if (Input.GetKey(resetKey))
-        {
-            keyHoldTimer += Time.deltaTime;
-
-            if (keyHoldTimer >= dotCount + 1)
-            {
-                resetText.text += ".";
-                dotCount++;
-            }
-
-            if (keyHoldTimer >= resetHoldTime)
-            {
-                transform.position = initialPosition;
-                keyHoldTimer = 0f;
-                dotCount = 0;
-                resetText.text = "Espace (Continuer)";
-                if (timerCoroutine != null)
-                {
-                    StopCoroutine(timerCoroutine);
-                }
-                timerCoroutine = StartCoroutine(TimerRoutine());
-            }
-        }
-        else
-        {
-            keyHoldTimer = 0f;
-            dotCount = 0;
-            resetText.text = "Espace (Continuer)";
-        }
     }
 
     IEnumerator TimerRoutine()
     {
-        float timer = timerDuration;
-        while (timer > 0)
-        {
-            timer -= Time.deltaTime;
-            yield return null;
-        }
+        float timer = timerDuration;        
+        yield return new WaitForSeconds(timerDuration);
         // scène suivante
     }
 }
