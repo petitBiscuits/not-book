705f15247d6467286c1a89bc4ef8ef9e2b682d8a
petitBiscuits <loic.frossard01@gmail.com>
2023-04-22 17:06:49 +0200

    add first set of level

---
+++ NotBook/Assets/UI Toolkit/Manager/UIManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIManager : VisualElement
+{
+
+    public class UXMLFactory : UxmlFactory<UIManager, UxmlTraits> { }
+
+    public UIManager()
+    {
+        
+    }
+
+    
+}
---
+++ NotBook/Assets/_Scripts/Player/NewBehaviourScript1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+
+public class PlayerManager : MonoBehaviour
+{
+    public static PlayerManager Instance;
+    
+
+    public void Awake()
+    {
+        Instance = this;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
